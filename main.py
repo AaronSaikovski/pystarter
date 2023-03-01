9a8b69665b81bb8c2dab769f9967ea5fc03054e0
Aaron Saikovski <asaikovski@microsoft.com>
2023-03-01 14:54:50 +1100

    added v1.0 of the initial code

---
+++ main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+""" Short description of this Python module.
+
+Longer description of this module.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "One solo developer"
+__authors__ = ["One developer", "And another one", "etc"]
+__contact__ = "mail@example.com"
+__copyright__ = "Copyright $YEAR, $COMPANY_NAME"
+__credits__ = ["One developer", "And another one", "etc"]
+__date__ = "YYYY/MM/DD"
+__deprecated__ = False
+__email__ =  "mail@example.com"
+__license__ = "GPLv3"
+__maintainer__ = "developer"
+__status__ = "Production"
+__version__ = "0.0.1"
+
+
+def main():
+    """Example function with types documented in the docstring.
+
+    `PEP 484`_ type annotations are supported. If attribute, parameter, and
+    return types are annotated according to `PEP 484`_, they do not need to be
+    included in the docstring:
+
+    Parameters
+    ----------
+    param1 : int
+        The first parameter.
+    param2 : str
+        The second parameter.
+
+    Returns
+    -------
+    bool
+        True if successful, False otherwise.
+
+    .. _PEP 484:
+        https://www.python.org/dev/peps/pep-0484/
+
+    
+    """
+    print("This is the main method.")
+
+if __name__ == "__main__":
+    main()
+    
