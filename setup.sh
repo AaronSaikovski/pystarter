5f4a23417051e5992de0b904644e04fe3beda7d9
AaronSaikovski <asaikovski@outlook.com>
2023-04-27 08:52:00 +1000

    added setup .sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#PIP Install
 #setup the virtual environment
 python3 -m venv .venv 
 
@@ -11,3 +12,19 @@ pip install -r requirements.txt
 
 #upgrade PIP
 pip install --upgrade pip
+
+
+#Miniconda setup
+
+#To create an environment - Replace myenv with the environment name
+#specifying python will install python latest version 
+conda create --name <myenv> python
+
+#Activate a given environment
+conda activate <envname> 
+
+#Export your active environment to a new file - make sure the environment is activated first!!
+conda env export > environment.yml
+
+#to install a package via Conda
+conda install <PACKAGE_NAME>
